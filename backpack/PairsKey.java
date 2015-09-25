8798a4453182fa0d3c9299df10fa83265719ffc9
Victor Lee <likaiwalkman@qq.com>
2015-09-25 17:11:37 +0800

    Create PairsKey.java

---
+++ backpack/PairsKey.java
@@ -0,0 +1,22 @@
+package backpack;
+
+public class PairsKey {
+	private int i;
+	private int j;
+	public PairsKey(int i, int j) {
+		this.i = i;
+		this.j = j;
+	}
+	@Override
+	public int hashCode() {
+		return i*j;
+	}
+	@Override
+	public boolean equals( Object obj ) {
+		if ( !(obj instanceof PairsKey) ) {
+			return false;
+		} else {
+			return ((PairsKey)obj).i == i && ((PairsKey)obj).j == j;
+		}
+	}
+}
