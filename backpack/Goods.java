b984f4ba3198c2e6d8fbef4707301d2847b17346
Victor Lee <likaiwalkman@qq.com>
2015-09-25 17:10:56 +0800

    Create Goods.java

---
+++ backpack/Goods.java
@@ -0,0 +1,29 @@
+package backpack;
+
+public class Goods {
+
+	public Goods(int weight, int value) {
+		this.weight = weight;
+		this.value = value;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight( int weight ) {
+		this.weight = weight;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue( int value ) {
+		this.value = value;
+	}
+
+	private int	value;
+	private int	weight;
+
+}
