490ec119eed95d6d8557ec93a3683f1da616222b
unknown <kai_li@DST63468.cn1.global.ctrip.com>
2016-02-03 19:28:34 +0800

    encore

---
+++ src/main/java/classloader/dynamicproxy/samples/MyClassLoader.java
@@ -0,0 +1,10 @@
+package classloader.dynamicproxy.samples;
+
+public class MyClassLoader extends ClassLoader {
+
+    public Class<?> defineMyClass(byte[] b, int off, int len) {
+        //"classloader.dynamicproxy.samples.Programmer1",
+        return super.defineClass(b, off, len);
+    }
+
+}
---
+++ src/main/java/classloader/dynamicproxy/samples/Programmer.java
@@ -0,0 +1,7 @@
+package classloader.dynamicproxy.samples;
+
+public class Programmer {
+    public void code() {
+        System.out.println("I'm a Programmer,Just Coding.....");
+    }
+}
