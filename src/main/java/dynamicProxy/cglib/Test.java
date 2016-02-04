25ee5f47d140d114fb7160bc8e2f2cf9e81904e7
unknown <kai_li@DST63468.cn1.global.ctrip.com>
2016-02-04 15:36:02 +0800

    encore

--- src/main/java/dynamicProxy/cglib/Test.java
+++ src/main/java/dynamicProxy/cglib/Test.java
@@ -4,7 +4,7 @@ import net.sf.cglib.proxy.Enhancer;
 
 public class Test {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalAccessException, InstantiationException {
         Programmer progammer = new Programmer();
 
         Hacker hacker = new Hacker();
@@ -13,6 +13,14 @@ public class Test {
         //设置要创建动态代理的类
         enhancer.setSuperclass(progammer.getClass());
         // 设置回调，这里相当于是对于代理类上所有方法的调用，都会调用CallBack，而Callback则需要实行intercept()方法进行拦截
+
+        //Method 1:failed
+        /*
+        enhancer.setCallbackType(Hacker.class);
+        Class enhancerClass = enhancer.createClass();
+        Programmer proxy = (Programmer) enhancerClass.newInstance();*/
+
+        //Method 2:success
         enhancer.setCallback(hacker);
         Programmer proxy = (Programmer) enhancer.create();
         proxy.code();
