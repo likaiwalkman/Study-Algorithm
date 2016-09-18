afb8288ec9dcfd21126b9eabc0036bda0bd91422
likai <likai@hys-inc.cn>
2016-09-18 09:52:43 +0800

    add junit

--- src/main/java/nio/BufferWriteReadUsageTest.java
+++ src/main/java/nio/BufferWriteReadUsageTest.java
@@ -1,9 +1,9 @@
 package nio;
 
+import org.junit.Test;
+
 import java.io.IOException;
 import java.nio.ByteBuffer;
-import java.nio.channels.Channel;
-import java.nio.channels.SocketChannel;
 
 public class BufferWriteReadUsageTest {
     public static void main(String[] args) throws IOException {
@@ -36,5 +36,41 @@ public class BufferWriteReadUsageTest {
         buffer.compact();
 
         buffer.position();
+    }
+
+    @Test
+    public void testPut(){
+        int capacity = 128;
+        ByteBuffer buffer = ByteBuffer.allocate(capacity);
+        System.out.printf("position:%s, limit:%s, capacity:%s \n",buffer.position(), buffer.limit(), buffer.capacity());
+        buffer.put((byte)1);
+        System.out.printf("position:%s, limit:%s, capacity:%s \n",buffer.position(), buffer.limit(), buffer.capacity());
+    }
+
+    @Test
+    public void testGet(){
+        int capacity = 128;
+        ByteBuffer buffer = ByteBuffer.allocate(capacity);
+        buffer.put((byte)1);
+        System.out.printf("position:%s, limit:%s, capacity:%s \n",buffer.position(), buffer.limit(), buffer.capacity());
+        buffer.flip();
+        System.out.printf("position:%s, limit:%s, capacity:%s \n",buffer.position(), buffer.limit(), buffer.capacity());
+        buffer.get();
+        System.out.printf("position:%s, limit:%s, capacity:%s \n",buffer.position(), buffer.limit(), buffer.capacity());
+    }
+
+    @Test
+    public void testCompact(){
+        int capacity = 128;
+        ByteBuffer buffer = ByteBuffer.allocate(capacity);
+        buffer.put((byte)1);
+        buffer.put((byte)1);
+        System.out.printf("position:%s, limit:%s, capacity:%s \n",buffer.position(), buffer.limit(), buffer.capacity());
+        buffer.flip();
+        System.out.printf("position:%s, limit:%s, capacity:%s \n",buffer.position(), buffer.limit(), buffer.capacity());
+        buffer.get();
+        System.out.printf("position:%s, limit:%s, capacity:%s \n",buffer.position(), buffer.limit(), buffer.capacity());
+        buffer.compact();
+        System.out.printf("position:%s, limit:%s, capacity:%s \n",buffer.position(), buffer.limit(), buffer.capacity());
     }
 }
