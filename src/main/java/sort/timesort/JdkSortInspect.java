6091bdf3afa6d3a15974bc8a52387059689d490c
likai <likai@hys-inc.cn>
2017-01-10 13:04:46 +0800

    copy sort algorithms to outer folder

---
+++ src/main/java/sort/timesort/JdkSortInspect.java
@@ -0,0 +1,22 @@
+package sort.timesort;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class JdkSortInspect {
+    public static void main(String[] args) {
+        int[] arr = {4, 1, 5, 2, 3};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+        List<String> ls = new ArrayList<String>();
+        ls.add("4");
+        ls.add("1");
+        ls.add("5");
+        ls.add("2");
+        ls.add("3");
+        Collections.sort(ls);
+        System.out.println(ls);
+    }
+}
