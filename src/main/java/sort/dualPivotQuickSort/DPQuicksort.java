623c62d0e17269bfa6c4ead01b9186431034a9ac
likai <likai@hys-inc.cn>
2017-01-12 18:23:04 +0800

    modify test case

--- src/main/java/sort/dualPivotQuickSort/DPQuicksort.java
+++ src/main/java/sort/dualPivotQuickSort/DPQuicksort.java
@@ -29,6 +29,7 @@ public class DPQuicksort {
     }
 
     private static void dualPivotQuicksort(int[] a, int left, int right, int div) {
+        int z[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30};
         int len = right - left;
         if (len < 27) { // insertion sort for tiny array
             for (int i = left + 1; i <= right; i++) {
