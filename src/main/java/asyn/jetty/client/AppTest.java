38eaebef18846a08048b7f4941ba9e8a2997aa6b
likai <likai@hys-inc.cn>
2017-02-19 00:29:54 +0800

    no comment

--- src/main/java/asyn/jetty/client/AppTest.java
+++ src/main/java/asyn/jetty/client/AppTest.java
@@ -12,7 +12,7 @@ import java.io.PrintStream;
 public class AppTest {
     public static void main(String[] args) throws Exception {
         String PATH = "/Users/victor/git/out.txt";
-        //System.setOut(new PrintStream(new FileOutputStream(PATH)));
+        System.setOut(new PrintStream(new FileOutputStream(PATH)));
 
         HttpClient client = new HttpClient();
         client.setConnectBlocking(false);
@@ -20,38 +20,39 @@ public class AppTest {
         client.setStopTimeout(1000);
         client.setMaxConnectionsPerDestination(1000);
         client.setMaxRequestsQueuedPerDestination(10000);
-        client.setRequestBufferSize(2*1024);
+        client.setRequestBufferSize(2 * 1024);
         client.setExecutor(Executors.newFixedThreadPool(50));
         client.start();
 
         for (int i = 1; i <= 10000; i++) {
 
-            System.err.println(i+" enqueue");
+            System.err.println(i + " enqueue");
 //        }
-            client.newRequest("http://www.baidu.com/img/baidu_jgylogo3.gif")
+            client.newRequest("http://localhost:8080/")
                     .onRequestFailure(new Request.FailureListener() {
                         public void onFailure(Request request, Throwable failure) {
-                            System.out.println("qf "+System.currentTimeMillis());
+                            System.out.println("qf " + System.currentTimeMillis());
                         }
                     })
                     .onRequestSuccess(new Request.SuccessListener() {
                         public void onSuccess(Request request) {
-                            System.out.println("qs "+System.currentTimeMillis());
+                            System.out.println("qs " + System.currentTimeMillis());
                         }
                     })
                     .onResponseFailure(new Response.FailureListener() {
                         public void onFailure(Response response, Throwable failure) {
-                            System.out.println("sf "+System.currentTimeMillis());
+                            System.out.println("sf " + System.currentTimeMillis());
                         }
                     })
                     .onResponseSuccess(new Response.SuccessListener() {
                         public void onSuccess(Response response) {
-                            System.out.println("ss "+System.currentTimeMillis());
+                            System.out.println("ss " + System.currentTimeMillis());
                         }
                     })
                     .send(new Response.CompleteListener() {
                         public void onComplete(Result result) {
-                            System.out.println("cp "+System.currentTimeMillis());
+                            //result.getResponse().
+                            System.out.println("cp " + System.currentTimeMillis());
                         }
                     });
         }
--- src/main/java/asyn/jetty/client/SynReqTest.java
+++ src/main/java/asyn/jetty/client/SynReqTest.java
@@ -9,15 +9,23 @@ import org.apache.http.params.CoreConnectionPNames;
 import java.io.IOException;
 import java.util.Date;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class SynReqTest {
-    static String PATH = "http://www.baidu.com";
+    static String PATH = "http://localhost:8080";
+
     public static void main(String[] args) throws IOException, InterruptedException {
-        ExecutorService executor = Executors.newFixedThreadPool(50);
         int             times    = 1000;
+
+        long now = System.currentTimeMillis();
+        for (int i = 0; i < times; i++) {
+            new Req(null).run();
+        }
+        System.out.println(System.currentTimeMillis() - now);
+
+        //System.exit(0);
+        ExecutorService executor = Executors.newFixedThreadPool(10);
         long            time     = System.currentTimeMillis();
         CountDownLatch  latch    = new CountDownLatch(times);
         System.out.println(new Date());
@@ -26,26 +34,30 @@ public class SynReqTest {
         }
         latch.await();
         executor.shutdown();
-        System.out.println(times+"times cost mills "+(System.currentTimeMillis()-time));
+        System.out.println(times + "times cost mills " + (System.currentTimeMillis() - time));
     }
-    static class Req implements Runnable{
+
+    static class Req implements Runnable {
         private CountDownLatch latch;
-        public Req(CountDownLatch latch){
+
+        public Req(CountDownLatch latch) {
             this.latch = latch;
         }
 
         @Override
         public void run() {
-            try{
+            try {
                 HttpClient client = new DefaultHttpClient();
-                client.getParams().setParameter(CoreConnectionPNames.CONNECTION_TIMEOUT,5000);
-                HttpResponse response = client.execute(new HttpGet(PATH));
-                int statusCode = response.getStatusLine().getStatusCode();
-                //System.out.println(""+statusCode +" " + new Date());
-            } catch (Exception e){
-                System.out.println("ERR"+e);
-            }finally {
-                latch.countDown();
+                client.getParams().setParameter(CoreConnectionPNames.CONNECTION_TIMEOUT, 5000);
+                HttpResponse response   = client.execute(new HttpGet(PATH));
+                int          statusCode = response.getStatusLine().getStatusCode();
+                System.out.println(""+statusCode +" " + new Date());
+            } catch (Exception e) {
+                System.out.println("ERR " + e);
+            } finally {
+                if (latch != null) {
+                    latch.countDown();
+                }
             }
         }
     }
