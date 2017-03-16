ed6d90719f4ae04f5d6ddc24a7c5d88f4333a9f0
likai <likai@hys-inc.cn>
2017-03-16 10:53:30 +0800

    queued model to total file size

---
+++ src/main/java/pcotj/scalability/ConcurrentTotalFileSizeWQueue.java
@@ -0,0 +1,68 @@
+package pcotj.scalability;
+
+import java.io.File;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class ConcurrentTotalFileSizeWQueue {
+    final         AtomicLong          pendingFileVisits = new AtomicLong();
+    final private BlockingQueue<Long> fileSizes         = new ArrayBlockingQueue<Long>(500);
+    private ExecutorService service;
+
+    public static void main(final String[] args) throws InterruptedException {
+        final long start = System.nanoTime();
+        String fileName = "D:\\";
+        final long total = new ConcurrentTotalFileSizeWQueue().getTotalSizeOfFile(fileName);
+        final long end = System.nanoTime();
+        System.out.println("Total Size: " + total);
+        System.out.println("Time taken: " + (end - start) / 1.0e9);
+    }
+
+    private void startExploreDir(final File file) {
+        pendingFileVisits.incrementAndGet();
+        service.execute(new Runnable() {
+            public void run() {
+                exploreDir(file);
+            }
+        });
+    }
+
+    private void exploreDir(final File file) {
+        long fileSize = 0;
+        if (file.isFile())
+            fileSize = file.length();
+        else {
+            final File[] children = file.listFiles();
+            if (children != null)
+                for (final File child : children) {
+                    if (child.isFile())
+                        fileSize += child.length();
+                    else {
+                        startExploreDir(child);
+                    }
+                }
+        }
+        try {
+            fileSizes.put(fileSize);
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+        pendingFileVisits.decrementAndGet();
+    }
+
+    private long getTotalSizeOfFile(final String fileName)
+            throws InterruptedException {
+        service = Executors.newFixedThreadPool(100);
+        try {
+            startExploreDir(new File(fileName));
+            long totalSize = 0;
+            while (pendingFileVisits.get() > 0 || fileSizes.size() > 0) {
+                final Long size = fileSizes.poll(10, TimeUnit.SECONDS);
+                totalSize += size;
+            }
+            return totalSize;
+        } finally {
+            service.shutdown();
+        }
+    }
+}
