feb5ecc98c970a5a8794f83a5b71a0df295bab25
likai <likai@hys-inc.cn>
2017-04-06 19:03:10 +0800

    phantom reference proof

---
+++ src/main/java/gc/reference/PhantomNotifierTest.java
@@ -0,0 +1,31 @@
+package gc.reference;
+
+import java.lang.ref.PhantomReference;
+import java.lang.ref.Reference;
+import java.lang.ref.ReferenceQueue;
+import java.util.LinkedList;
+import java.util.List;
+
+public class PhantomNotifierTest {
+    public static void main(String[] args) {
+        List<String> list = new LinkedList<String>();
+        list.add("lilei");
+
+        ReferenceQueue<List<String>> referenceQueue = new ReferenceQueue<List<String>>();
+        PhantomReference<List<String>> phantomReference = new PhantomReference<List<String>>(list, referenceQueue);
+        int count = 0;
+        while (true) {
+            Reference<? extends List<String>> poll = referenceQueue.poll();
+            if ( poll == null) {
+                System.out.println("not collected");
+            } else {
+                System.out.println("collected"+poll);
+            }
+            if (count == 3) {
+                list = null;
+            } else if (count == 10){
+                break;
+            }
+        }
+    }
+}
