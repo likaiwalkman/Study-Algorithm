26aaa0abfea47433ad54405625e151c0defa9b6e
Victor Lee <likaiwalkman@qq.com>
2020-05-09 19:13:59 +0800

    Create App.java

---
+++ src/main/java/designPattern/App.java
@@ -0,0 +1,8 @@
+public class App {
+
+    public static void main(String[] args) {
+        Visitor visitor = new VisitorPrinter();
+        Node nodeA = new NodeA();
+        nodeA.accept(visitor);
+    }
+}
