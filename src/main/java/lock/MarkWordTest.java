32299263ff110ea80ffa55989e4312e88aba0fb9
likai <likai@hys-inc.cn>
2023-07-09 00:10:59 +0800

    feat: add more algo

---
+++ src/main/java/lock/MarkWordTest.java
@@ -0,0 +1,10 @@
+package lock;
+
+import org.openjdk.jol.info.ClassLayout;
+
+public class MarkWordTest {
+    public static void main(String[] args) {
+        Object obj = new Object();
+        System.out.println(ClassLayout.parseInstance(obj).toPrintable());
+    }
+}
