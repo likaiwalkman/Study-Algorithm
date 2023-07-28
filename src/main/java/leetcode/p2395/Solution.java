ad03511b95bba98c3cb3102f3cb45fc95f2d6b6e
likai <likai@hys-inc.cn>
2023-07-28 18:18:04 +0800

    chore: add more algo

--- src/main/java/leetcode/p2395/Solution.java
+++ src/main/java/leetcode/p2395/Solution.java
@@ -1,5 +1,23 @@
 package leetcode.p2395;
 
-public class Solution {
+import java.util.HashSet;
+import java.util.Set;
 
+public class Solution {
+    public boolean findSubarrays(int[] nums) {
+        if (nums.length < 3) {
+            return false;
+        }
+
+        Set<Integer> set = new HashSet<>();
+        for (int k = 1; k < nums.length; k++) {
+            int v = nums[k-1]+nums[k];
+            if (set.contains(v)) {
+                return true;
+            }else {
+                set.add(v);
+            }
+        }
+        return false;
+    }
 }
