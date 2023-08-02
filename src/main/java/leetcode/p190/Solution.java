864333afb66480c53464c5952f1405bb94fc83d6
likai <likai@hys-inc.cn>
2023-08-02 23:02:29 +0800

    add rate limit demo

--- src/main/java/leetcode/p190/Solution.java
+++ src/main/java/leetcode/p190/Solution.java
@@ -3,37 +3,36 @@ package leetcode.p190;
 import java.util.Stack;
 
 public class Solution {
-    public static void main(String[] args) {
-        System.out.println(1<<30);
-        int x = 0xffffffff + 2;
-    }
     public int reverseBits(int n) {
-        int digits = 32;
-
-        if (n == 0){
-            return 0;
+        if (n == 0 || n == -1){
+            return n;
         }
+        Stack<Integer> stack = toStack(n);
+        Stack<Integer> reverse = reverse(stack);
+        int newInt = 0;
+        while (reverse.size() > 0) {
+            newInt = newInt << 1;
+            newInt = reverse.pop() | newInt;
+        }
+        return newInt;
+    }
+
+    public static Stack<Integer> reverse(Stack<Integer> stack){
+        Stack<Integer> result = new Stack<>();
+        while (stack.size() > 0) {
+            result.push(stack.pop());
+        }
+        return result;
+    }
+
+    public static Stack<Integer> toStack(int n){
+        int digits = 32;
         Stack<Integer> stack = new Stack<>();
         while (digits > 0){
             stack.push(n % 2 == 0 ? 0 : 1);
-            n = n >> 1;
+            n = n >>> 1;
             digits--;
         }
-        Stack<Integer> reverseStack = new Stack<>();
-        while (stack.size() > 0) {
-            reverseStack.push(stack.pop());
-        }
-        int newInt = 0;
-        while (reverseStack.size() > 0) {
-            newInt = newInt << 1;
-            Integer pop = reverseStack.pop();
-            if (newInt >= 0){
-                newInt += pop;
-            }else {
-                newInt -= pop;
-            }
-        }
-
-        return newInt;
+        return stack;
     }
 }
--- src/main/java/leetcode/p190/Test.java
+++ src/main/java/leetcode/p190/Test.java
@@ -5,6 +5,7 @@ import org.junit.Assert;
 public class Test {
     @org.junit.Test
     public void test1(){
+        Assert.assertTrue(-1 >>> 1 == Integer.MAX_VALUE);
         int i = new Solution().reverseBits(43261596);
         Assert.assertEquals(964176192, i);
     }
@@ -17,6 +18,8 @@ public class Test {
     @org.junit.Test
     public void test3(){
         int x1 = 0xfffffffd;
+        int x2 = 0xffffffff >>> 1;
+        int x3 = 0x7fffffff;
         int x = 0xffffffff + 2;
         int i = new Solution().reverseBits(x);
         System.out.println(i);
