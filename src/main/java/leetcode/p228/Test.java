183d3fcf925ef9eb17b33a5175ae376eed6626e9
likai <likai@hys-inc.cn>
2023-08-03 23:04:47 +0800

    add rate limit demo

--- src/main/java/leetcode/p228/Test.java
+++ src/main/java/leetcode/p228/Test.java
@@ -1,11 +1,16 @@
 package leetcode.p228;
 
+import org.junit.Assert;
+
 import java.util.List;
 
 public class Test {
+
     @org.junit.Test
     public void test1(){
         List<String> strings = new Solution().summaryRanges(new int[]{0, 2, 3, 4, 6, 8, 9});
+        String exp = "[0,2->4,6,8->9]";
+        Assert.assertEquals(exp, strings.toString().replace(" ", ""));
         System.out.println(strings);
     }
 }
