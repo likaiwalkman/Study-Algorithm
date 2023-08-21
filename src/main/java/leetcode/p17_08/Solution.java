84747bfabcf9f0d3933d74541a4d0f2c43824d1b
likai <likai@hys-inc.cn>
2023-08-21 16:31:46 +0800

    add more algo

--- src/main/java/leetcode/p17_08/Solution.java
+++ src/main/java/leetcode/p17_08/Solution.java
@@ -25,26 +25,29 @@ public class Solution {
     }
 
     public int lengthOfLIS(int[] nums) {
-        int[] dp = new int[nums.length];
-        dp[0] = 1;
-
-        for (int i = 1; i < nums.length; i++) {
-            int temp = 1;
-            for (int j = 0; j < i; j++) {
-                if (nums[i] > nums[j]){
-                    if (dp[j]+1 > temp) {
-                        temp = dp[j]+1;
+        int len = 1, n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        int[] d = new int[n + 1];
+        d[len] = nums[0];
+        for (int i = 1; i < n; ++i) {
+            if (nums[i] > d[len]) {
+                d[++len] = nums[i];
+            } else {
+                int l = 1, r = len, pos = 0;
+                while (l <= r) {
+                    int mid = (l + r) >> 1;
+                    if (d[mid] < nums[i]) {
+                        pos = mid;
+                        l = mid + 1;
+                    } else {
+                        r = mid - 1;
                     }
                 }
-            }
-            dp[i] = temp;
-        }
-        int max = 1;
-        for (int i = 1; i < dp.length; i++) {
-            if (dp[i] > max) {
-                max = dp[i];
+                d[pos + 1] = nums[i];
             }
         }
-        return max;
+        return len;
     }
 }
