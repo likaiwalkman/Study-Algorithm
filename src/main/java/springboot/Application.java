e0277a27db3b730ed14f19857025041e80e24023
likai <likai@hys-inc.cn>
2023-09-11 14:28:03 +0800

    add more algo

--- src/main/java/springboot/Application.java
+++ src/main/java/springboot/Application.java
@@ -8,6 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-        Tomcat tomcat = new Tomcat();
+        //Tomcat tomcat = new Tomcat();
     }
 }
